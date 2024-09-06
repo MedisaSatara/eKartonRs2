5eb001e65110cab340eca2437b71c76401944579
MedisaSatara <92315913+MedisaSatara@users.noreply.github.com>
2024-09-06 20:53:46 +0200

    updated program.cs

--- eKarton/eKarton/Program.cs
+++ eKarton/eKarton/Program.cs
@@ -108,4 +108,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<eKartonContext>();
+    dataContext.Database.Migrate();
+}
+
 app.Run();
